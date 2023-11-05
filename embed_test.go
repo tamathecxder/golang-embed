e3a5e9f30910a45ae4fc82a9b4512aaefa946af3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-05 20:06:05 +0700

    Use 'go embed' to embed 'golang_logo.png' as a byte slice

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -11,4 +13,15 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed golang_logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := os.WriteFile("golang_logo_new.png", logo, fs.ModePerm)
+
+	if err != nil {
+		panic(err)
+	}
 }
